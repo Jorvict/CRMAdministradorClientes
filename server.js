e9a8a47fcdfd28de3dc951311a533da318c63c89
Jorvict <jorvict15@gmail.com>
2023-05-15 08:14:32 -0500

    Se modificó ruta DB

--- server.js
+++ server.js
@@ -4,22 +4,15 @@ const jsonServer = require('json-server');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 app.use(cors()); // Agrega el middleware de CORS
-
-app.use(jsonServer.router('./db.json'));
-
-app.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
-
 //
 
 const server = jsonServer.create();
-// const router = jsonServer.router('./db.json');
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
  
 server.use(middlewares);
 server.use(router);
